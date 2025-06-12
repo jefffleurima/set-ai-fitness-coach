7da125222fdeb26edda43a38d06a28334d711b7d
jefffleurima <jefffleurima15@icloud.com>
2025-06-12 12:20:18 -0400

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Jeff Fleurima on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
